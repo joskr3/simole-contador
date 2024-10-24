a996cf3216ac618934202138fd52d0574a0a2e1a
Josue Patricio <josretamozovar@gmail.com>
2024-10-23 19:50:21 -0500

    trabajando con hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import './App.css'
+import { useState } from 'react'
+
+
+
+// vamos a hacer una funcion que me limite a trabajar con numeros naturales(mayor a 0)
+//y se pasamos como argumento al onclick
+
 
 function App() {
-
+  const [valor, setValor] = useState(0)
   return (
-    <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <main className='flex flex-col h-dvh w-dvw'>
+      <div className='max-w-[80%] m-auto border border-green-600 p-6 w-8/12 h-[50%]'>
+        <h1 className='text-7xl text-green-700 my-4'>Contador</h1>
+        <section>
+          {/* el numero que va cambiando */}
+          <p className='text-5xl font-bold my-4'>{valor}</p>
+        </section>
+        <section className=' flex my-6 justify-center'>
+          <button className='text-4xl border border-green-600 rounded-xl p-4 mx-3' onClick={() => { setValor(valor + 1) }}>
+            ➕
+          </button>
+          {/* los botones de + y -  */}
+          <button className='text-4xl border border-green-600 rounded-xl p-4 mx-3' onClick={() => { setValor(valor - 1) }}>
+            ➖
+          </button>
+        </section>
+      </div>
+    </main>
   )
 }
 
